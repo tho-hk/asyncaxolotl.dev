eb3dae48cae8f902c87a91a50f08120f9dcdcb4e
tho-hk <taine.ho@redteam101.com>
2025-09-07 01:41:44 +0800

    restarts project

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Our Multithread DEVenture</h1>
-      <p>Stay Async, stay adorable!</p>
-    </div>
-  )
-}
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface BlogPost {
-    title: string
-    slug: string
-    content: string
-    date: string
-}
